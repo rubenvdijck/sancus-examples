d04610b077b1f2dfb66c17963f9ae84b0efc2034
Ruben Van Dijck <ruben.vandijck1@student.kuleuven.be>
2025-01-03 19:52:33 +0100

    organising repo

--- ruben-examples/auth-ex-break/main.c
+++ ruben-examples/auth-ex-break/main.c
@@ -1,100 +1,107 @@
 #include <msp430.h>
 #include <stdio.h>
 #include <sancus/sm_support.h>
-#include <sancus_support/sm_io.h>
-#include <sancus_support/timer.h>
-#include <sancus_support/tsc.h>
 #include <sancus/reactive.h>
-#include <sancus_support/sm_control.h>
-#include <sancus_support/global_symtab.h>
+#include <sancus_support/sm_io.h>
 
-typedef tsc_t secret_data_t;
+#define CONN_ID 0
+#define AD_SIZE 6
+// NOTE: because of a known bug in the Sancus crypto engine, manually align buffers here
+uint8_t __attribute__((aligned(2))) cipher[SANCUS_KEY_SIZE];
+uint8_t __attribute__((aligned(2))) tag[SANCUS_TAG_SIZE];
+uint8_t __attribute__((aligned(2))) ad[AD_SIZE] = {0x0};
+uint8_t __attribute__((aligned(2))) key[SANCUS_KEY_SIZE] = 
+    #if SANCUS_KEY_SIZE == 8
+        { 0x83, 0x53, 0x6b, 0xf3, 0x07, 0x1f, 0xec, 0x8b };
+    #else
+        { 0x83, 0x53, 0x6b, 0xf3, 0x07, 0x1f, 0xec, 0x8b, 0xc1, 0x7c, 0xb2, 0x16, 0xca, 0xc5, 0x9a, 0x5e};
+    #endif
 
-typedef struct
+/* ********************************************************************** */
+DECLARE_SM(ping, 0x1234);
+SM_OUTPUT(ping, ping_produce_output);
+uint16_t SM_ENTRY(ping) __sm_ping_set_key(const uint8_t* ad,
+                            const uint8_t* cipher, const uint8_t* tag, uint16_t *conn_idx);
+
+void SM_ENTRY(ping) ping_gen_key(void)
 {
-    char cipher[sizeof(secret_data_t)];
-    char tag[SANCUS_TAG_SIZE];
-} CipherData;
-
-typedef unsigned nonce_t;
-
-void exit_success(void);
-
-/* ======== HELLO WORLD SM ======== */
-
-DECLARE_SM(hello, 0x1234);
-
-SM_OUTPUT(hello, output);
-
-SM_INPUT(hello, super_secure_ecall, data, len)
-{
-    pr_info("should never see this!");
+    sancus_wrap(ad, AD_SIZE, key, SANCUS_KEY_SIZE, cipher, tag);
 }
 
+void SM_ENTRY(ping) enter_ping(void)
+{
+    uint16_t data = 0xAABB;
+    puts("[ping] producing auth output..");
+    ASSERT( ping_produce_output((uint8_t*) &data, sizeof(data)) == 0);
+}
 
-/* ======== UNTRUSTED CONTEXT ======== */
+/* ********************************************************************** */
+DECLARE_SM(pong, 0x1234);
 
-void reactive_handle_output(uint16_t conn_id, void* data, size_t len) {
-    uint16_t args[] = {0, (uint16_t)data, (uint16_t)len};
-    uint16_t retval = 0;
-    uint16_t id = 0;
-    sm_call_module(&hello, 0x3, args, 3, &retval);
+uint16_t SM_ENTRY(pong) __sm_pong_set_key(const uint8_t* ad,
+                            const uint8_t* cipher, const uint8_t* tag, uint16_t *conn_idx);
+
+void SM_ENTRY(pong) pong_gen_key(void)
+{
+    sancus_wrap(ad, AD_SIZE, key, SANCUS_KEY_SIZE, cipher, tag);
+}
+
+SM_INPUT(pong, pong_process_input, data, len)
+{
+    puts("[pong] hi with auth input:");
+    dump_buf(data, len, "\tdata");
+}
+
+uint16_t SM_ENTRY(pong) __sm_pong_handle_input(uint16_t conn_idx,
+                                         const void* payload, size_t len);
+
+/* ********************************************************************** */
+
+void reactive_handle_output(conn_index conn_id, void* data, size_t len)
+{
+    /* dummy implementation: since there's only one connection, we don't need
+     * to do any routing and can simply hardcode the forwarding here */
+    puts("[untrusted] forwarding auth output from ping to pong..");
+    dump_buf(data, len, "\tdata");
+    int rv = __sm_pong_handle_input(CONN_ID, data, len);
+    printf("[untrusted] pong returned %d\n", rv);
+    ASSERT( rv == 0);
 }
 
 int main()
 {
+    uint16_t rv_idx = -1;
     msp430_io_init();
-    sancus_enable(&hello);
 
-    uint tsc1, tsc2;
-    timer_tsc_start();
-    tsc1 = timer_tsc_end();
-    pr_info1("tsc overhead: %u\n", tsc1);
-    
-    CipherData cipher = { .cipher = "\x78\x56\x78\x56\x78\x56\x78\x56",
-                        .tag = "\xe7\x1c\x2f\x8e\x29\xba\x6f\xfc\xd0\x36\x94\x83\xb2\x77\xb2\x9c" };
-    size_t len = sizeof(CipherData);
-    timer_tsc_start();
-    // __sm_hello_handle_input(1, &cipher, len);
-    tsc2 = timer_tsc_end();
-    pr_info2("Time to verify: %u, tsc overhead: %u\n", tsc2, tsc1);
+    puts("[untrusted] enabling ping/pong SMs..");
+    sancus_enable(&ping);
+    pr_sm_info(&ping);
+    sancus_enable(&pong);
+    pr_sm_info(&pong);
 
-    // ======== WITHOUT INFO ==========
+    puts("\n---");
+    puts("[untrusted] setting SM keys..");
+    ping_gen_key();
+    dump_buf(key, SANCUS_KEY_SIZE, "\tkey");
+    dump_buf(ad, AD_SIZE, "\tad");
+    dump_buf(cipher, SANCUS_KEY_SIZE, "\tcipher");
+    dump_buf(tag, SANCUS_TAG_SIZE, "\ttag");
+    ASSERT( __sm_ping_set_key(ad, cipher, tag, &rv_idx) == 0);
+    printf("[untrusted] connection idx is %d\n", rv_idx);
+    ASSERT( rv_idx == CONN_ID);
 
-    // CipherData cipher_guess = { .cipher = "\x78\x56\x78\x56\x78\x56\x78\x56",
-    //                     .tag = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" };
+    pong_gen_key();
+    dump_buf(key, SANCUS_KEY_SIZE, "\tkey");
+    dump_buf(ad, AD_SIZE, "\tad");
+    dump_buf(cipher, SANCUS_KEY_SIZE, "\tcipher");
+    dump_buf(tag, SANCUS_TAG_SIZE, "\ttag");
+    ASSERT( __sm_pong_set_key(ad, cipher, tag, &rv_idx) == 0);
+    printf("[untrusted] connection idx is %d\n", rv_idx);
+    ASSERT( rv_idx == CONN_ID);
 
-    // // SANCUS_SECURITY=128
-    // for ( int e = 0; e < 8; e++ ){
-    //     for( int i = 0; i < 256; i++ ){
-    //         cipher_guess.tag[2*e] = i;
-    //         for( int j = 0; j < 256; j++ ){
-    //             cipher_guess.tag[2*e+1] = j;
-                
-    //             timer_tsc_start();
-    //             super_secure_ecall(no, &cipher_guess);
-    //             tsc2 = timer_tsc_end();
-    //             if( tsc2 > 2553 + e*173 ){
-    //                 dump_buf((uint8_t*)cipher_guess.tag, 16, "  Guessed tag");
-    //                 pr_info1("Time to verify guess: %u\n", tsc2);
-    //                 break;
-    //             }
-    //         }
-    //         pr_info1("Finished %d/256\n", i+1);
-    //         if( tsc2 > 2553 + e*173 ){
-    //             dump_buf((uint8_t*)cipher_guess.tag, 16, "  Guessed tag");
-    //             pr_info1("Time to verify guess: %u\n", tsc2);
-    //             break;
-    //         }
-    //     }
-    //     pr_info1("Finished %d/8\n", e+1);
-    // }
+    puts("\n---");
+    puts("[untrusted] calling ping SM..");
+    enter_ping();
 
-    exit_success();
-}
-
-void exit_success(void)
-{
-    pr_info("SM disabled; all done!\n\n");
     FINISH();
 }
